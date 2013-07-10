9073a622b9171bdae6467266de2e1f88fa6db625
Sean Zhang <zxy1256@gmail.com>
2013-07-10 04:44:35 +0000

    Update web.js

--- web.js
+++ web.js
@@ -1,9 +1,13 @@
+var fs = require('fs');
+
+var buffer = fs.readFileSync('index.html');
+console.log(buffer.toString('utf-8'));
 var express = require('express');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(buffer.toString('utf-8'));
 });
 
 var port = process.env.PORT || 5000;
